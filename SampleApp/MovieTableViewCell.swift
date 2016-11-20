736441aa94405ebbe55e1a44e5150968845959a6
Chandra <chandra.asar@gmail.com>
2016-11-20 13:37:36 -0500

    good progress with movie app table view, so pushing code up now before continuing to finish

---
+++ SampleApp/MovieTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieTableViewCell.swift
+//  SampleApp
+//
+//  Created by Asar, Chandra on 11/20/16.
+//  Copyright © 2016 Asar, Chandra. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieYearLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SampleApp/MovieTableViewController.swift
+++ SampleApp/MovieTableViewController.swift
@@ -26,17 +26,24 @@ class MovieTableViewController: UITableViewController {
         self.getMovieData();
     }
     
+    //defines number of rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieTitles.count
     }
     
+    //defines number of cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = movieTitles[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MovieTableViewCell
         
-        cell.titleLabel.text = self.movieYears[indexPath.row]
+        cell.movieTitleLabel?.text = movieTitles[indexPath.row]
+        cell.movieYearLabel?.text = movieYears[indexPath.row]
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("yo");
+        //tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func getMovieData(){
